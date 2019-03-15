b7ea177d8aadd30c9824b254f958240daf899107
e <opfanswyx@outlook.com>
2019-03-15 11:04:07 +0800

    commit

--- httpd.c
+++ httpd.c
@@ -34,7 +34,7 @@
  * ***************************/
 #define ISspace(x) isspace((int)(x))
 
-#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n" //定义server名称
+#define SERVER_STRING "Server: jdbhttpd/0.1.0\r\n"  /* 定义server名称 */
 #define STDIN   0
 #define STDOUT  1
 #define STDERR  2
@@ -53,7 +53,6 @@ int startup(u_short *);
 void unimplemented(int);
 
 // Http请求，后续主要是处理这个头
-//
 // GET / HTTP/1.1
 // Host: 192.168.0.23:47310
 // Connection: keep-alive
@@ -63,7 +62,6 @@ void unimplemented(int);
 // Accept - Encoding: gzip, deflate, sdch
 // Accept - Language : zh - CN, zh; q = 0.8
 // Cookie: __guid = 179317988.1576506943281708800.1510107225903.8862; monitor_count = 5
-// 
 
 // POST / color1.cgi HTTP / 1.1
 // Host: 192.168.0.23 : 47310
@@ -83,28 +81,22 @@ void unimplemented(int);
 // color=gray
 
 
-/**********************************************************************/
-/* A request has caused a call to accept() on the server port to
- * return.  
+/**********************************************************************
+ * A request has caused a call to accept() on the server port to return.  
  * Process the request appropriately.
- * Parameters: the socket connected to the client 
- * 
- * 参数：连接到客户端的套接字
- * */
-/**********************************************************************/
+ * Parameters: the socket connected to the client(连接到客户端的套接字)
+***********************************************************************/
 void accept_request(void *arg)
-{
-    //intptr_t在不同的平台是不一样的，始终与地址位数相同
-    int client = (intptr_t)arg;
+{ 
+    int client = (intptr_t)arg; /* intptr_t在不同的平台是不一样的，始终与地址位数相同 */
     char buf[1024];
     size_t numchars;
     char method[255];
     char url[255];
     char path[512];
     size_t i, j;
-    struct stat st; //文件结构体，用于获取文件shux
-    int cgi = 0;      /* becomes true if server decides this is a CGI
-                       * program */
+    struct stat st;   /* 文件结构体，用于获取文件shux */
+    int cgi = 0;      /* becomes true if server decides this is a CGI program */
     char *query_string = NULL;
 
     /****************************
@@ -126,6 +118,7 @@ void accept_request(void *arg)
     }
     j=i;
     method[i] = '\0';
+    printf("LINE[%d]\tfunction[%s]\tmethod=[%s]\n",__LINE__,__FUNCTION__,method);
     /************************************
      * 函数说明：strcasecmp()用来比较参数s1 和s2 字符串，比较时会自动忽略大小写的差异。
      * 返回值：若参数s1 和s2 字符串相同则返回0。
@@ -133,24 +126,23 @@ void accept_request(void *arg)
      *        s1 长度若小于s2 长度则返回小于0 的值。
      * *********************************/
     if (strcasecmp(method, "GET") && strcasecmp(method, "POST"))
-    {
-        //如果接收到的数据中既有GET方法与POST方法，则向客户端返回该web方法尚未实现
-        unimplemented(client);
+    { 
+        unimplemented(client);  /* 如果接收到的数据中既有GET方法与POST方法，则向客户端返回该web方法尚未实现 */
         return;
     }
 
     if (strcasecmp(method, "POST") == 0)
-        cgi = 1;    //cgi为标志位，置1说明开启cgi解析
+        cgi = 1;    /* cgi为标志位，置1说明开启cgi解析 */
 
     i = 0;
-    while (ISspace(buf[j]) && (j < numchars))   //如果为空白字符则跳过
+    while (ISspace(buf[j]) && (j < numchars))   /* 如果为空白字符则跳过 */
         j++;
     /*********************************
      * //得到 "/"   
      * 注意：如果你的http的网址为http://x.x.x.x:8888/index.html 
      * 那么你得到的第一条http信息为GET /index.html HTTP/1.1，那么 
      * 解析得到的就是/index.html 
-     * ********************************/
+    **********************************/
     while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < numchars))
     {
         url[i] = buf[j];
@@ -161,22 +153,19 @@ void accept_request(void *arg)
     if (strcasecmp(method, "GET") == 0)
     {
         query_string = url;
-        //如果是GET请求，url可能会带有?,有查询参数
-        while ((*query_string != '?') && (*query_string != '\0'))
+        
+        while ((*query_string != '?') && (*query_string != '\0'))   /* 如果是GET请求，url可能会带有?,有查询参数 */
             query_string++;
         if (*query_string == '?')
         {
-            cgi = 1;
-            //将解析参数截取下来
-            *query_string = '\0';
+            cgi = 1;          
+            *query_string = '\0';    /* 将解析参数截取下来 */
             query_string++;
         }
     }
 
-    //url中的路径格式化到path
-    sprintf(path, "htdocs%s", url);
-    //如果path只是一个目录，默认设置为首页index.html
-    if (path[strlen(path) - 1] == '/')
+    sprintf(path, "htdocs%s", url);     /* url中的路径格式化到path */
+    if (path[strlen(path) - 1] == '/')  /* 如果path只是一个目录，默认设置为首页index.html */
         strcat(path, "index.html");
 
     /**************************************
@@ -185,36 +174,31 @@ void accept_request(void *arg)
      * 返回值: 执行成功则返回0，失败返回-1，错误代码存于errno（需要include <errno.h>）
      * ***********************************/
     if (stat(path, &st) == -1) {
-        //假如访问的网页不存在，则不断的读取剩下的请求头信息，并丢弃即可
+        /* 假如访问的网页不存在，则不断的读取剩下的请求头信息，并丢弃即可 */
         while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
             numchars = get_line(client, buf, sizeof(buf));
-        //最后返回网页不存在
-        not_found(client);
+        not_found(client);      /* 最后返回网页不存在 */
     }
     else
     {
-        //st_mode是用特征位来表示文件类型的
-        //S_IFMT      0170000     文件类型的位遮罩
-        //如果路径是个目录，那就将主页进行显示
-        if ((st.st_mode & S_IFMT) == S_IFDIR) //目录
+        /***************************************
+        * st_mode是用特征位来表示文件类型的
+        * S_IFMT      0170000     文件类型的位遮罩
+        * 如果路径是个目录，那就将主页进行显示
+        ***************************************/
+        if ((st.st_mode & S_IFMT) == S_IFDIR)   /* 目录 */
             strcat(path, "/index.html");
-        //如果你的文件默认是有执行权限的，自动解析成cgi程序，如果有执行权限但是不能执行，会接受到报错信号
-        if ((st.st_mode & S_IXUSR) ||
-                (st.st_mode & S_IXGRP) ||
-                (st.st_mode & S_IXOTH)    )
-                //S_IXUSR:文件所有者具可执行权限
-                //S_IXGRP:用户组具可执行权限
-                //S_IXOTH:其他用户具可读取权限 
+        /* 如果你的文件默认是有执行权限的，自动解析成cgi程序，如果有执行权限但是不能执行，会接受到报错信号 */
+        if ((st.st_mode & S_IXUSR) ||           /* S_IXUSR:文件所有者具可执行权限 */
+                (st.st_mode & S_IXGRP) ||       /* S_IXGRP:用户组具可执行权限 */
+                (st.st_mode & S_IXOTH)    )     /* S_IXOTH:其他用户具可读取权限 */     
             cgi = 1;
-        if (!cgi)
-            //读取静态文件返回给请求的http客户端
-            serve_file(client, path);
+        if (!cgi)    
+            serve_file(client, path);   /* 读取静态文件返回给请求的http客户端 */
         else
-            //执行cgi动态解析
-            execute_cgi(client, path, method, query_string);
+            execute_cgi(client, path, method, query_string);     /* 执行cgi动态解析 */
     }
-    //执行完毕后，关闭socket
-    close(client);
+    close(client);          /* 执行完毕后，关闭socket */
 }
 
 /**********************************************************************/
@@ -249,14 +233,12 @@ void cat(int client, FILE *resource)
     char buf[1024];
 
     fgets(buf, sizeof(buf), resource);
-    //循环读取
-    while (!feof(resource))
+    while (!feof(resource))         /* 循环读取 */
     {
         send(client, buf, strlen(buf), 0);
         fgets(buf, sizeof(buf), resource);
     }
 }
-
 /**********************************************************************/
 /* Inform the client that a CGI script could not be executed.
  * Parameter: the client socket descriptor. */
@@ -285,20 +267,17 @@ void error_die(const char *sc)
     perror(sc);
     exit(1);
 }
-
 /**********************************************************************/
 /* Execute a CGI script.  Will need to set environment variables as
- * appropriate.
+ * appropriate. (执行cgi动态解析)
  * Parameters: client socket descriptor
  *             path to the CGI script */
 /**********************************************************************/
-//执行cgi动态解析
 void execute_cgi(int client, const char *path,
         const char *method, const char *query_string)
 {
     char buf[1024];
-    //读写管道的声明
-    int cgi_output[2];
+    int cgi_output[2];      /* 读写管道的声明 */
     int cgi_input[2];
     pid_t pid;
     int status;
@@ -308,35 +287,33 @@ void execute_cgi(int client, const char *path,
     int content_length = -1;
 
     buf[0] = 'A'; buf[1] = '\0';
-    //如果是GET请求
-    //读取并且丢弃头信息
-    if (strcasecmp(method, "GET") == 0)
-        while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
+
+    if (strcasecmp(method, "GET") == 0)                     /* 如果是GET请求,读取并且丢弃头信息  */
+        while ((numchars > 0) && strcmp("\n", buf))         /* read & discard headers */
             numchars = get_line(client, buf, sizeof(buf));
-    else if (strcasecmp(method, "POST") == 0) /*POST*/
+    else if (strcasecmp(method, "POST") == 0)               /*POST*/
     {
         numchars = get_line(client, buf, sizeof(buf));
-        while ((numchars > 0) && strcmp("\n", buf))
-        {
-            //循环读取头信息找到Content-Length字段的值
-            //"Content-Length: 15"
-            buf[15] = '\0';
+        while ((numchars > 0) && strcmp("\n", buf))         /* 循环读取头信息找到Content-Length字段的值 */
+        {       
+            buf[15] = '\0';                                 /* "Content-Length: 15" */
             if (strcasecmp(buf, "Content-Length:") == 0)
                 content_length = atoi(&(buf[16]));
             numchars = get_line(client, buf, sizeof(buf));
         }
         if (content_length == -1) {
-            bad_request(client);    //错误请求
+            bad_request(client);    /* 错误请求 */
             return;
         }
     }
-    else/*HEAD or other*/
+    else    /*HEAD or other*/
     {
     }
     /****************************************
      * #include<unistd.h>
      * int pipe(int filedes[2]);
-     * 返回值：成功，返回0，否则返回-1。参数数组包含pipe使用的两个文件的描述符。fd[0]:读管道，fd[1]:写管道。
+     * 返回值：成功，返回0，否则返回-1。
+     * 参数数组包含pipe使用的两个文件的描述符。fd[0]:读管道，fd[1]:写管道。
      * 必须在fork()中调用pipe()，否则子进程不会继承文件描述符。
      * 两个进程不共享祖先进程，就不能使用pipe。但是可以使用命名管道。
      * pipe(cgi_output)执行成功后，cgi_output[0]:读通道 cgi_output[1]:写通道
@@ -355,25 +332,19 @@ void execute_cgi(int client, const char *path,
         return;
     }
 
-    //返回正确响应码200
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");        /* 返回正确响应码200 */
     send(client, buf, strlen(buf), 0);
     
-    if (pid == 0)  /* child: CGI script */
+    if (pid == 0)                               /* child: CGI script */
     {
         char meth_env[255];
         char query_env[255];
         char length_env[255];
-
-        //1代表着stdout，0代表着stdin，将系统标准输出重定向为cgi_output[1]
-        dup2(cgi_output[1], STDOUT);
-        //将系统标准输入重定向为cgi_input[0]，这一点非常关键，
-        //cgi程序中用的是标准输入输出进行交互
-        dup2(cgi_input[0], STDIN);
-        //关闭了cgi_output中的读通道
-        close(cgi_output[0]);
-        //关闭了cgi_input中的写通道
-        close(cgi_input[1]);
+   
+        dup2(cgi_output[1], STDOUT);       /* 1代表着stdout，0代表着stdin，将系统标准输出重定向为cgi_output[1] */
+        dup2(cgi_input[0], STDIN);         /* 将系统标准输入重定向为cgi_input[0]，这一点非常关键，cgi程序中用的是标准输入输出进行交互 */
+        close(cgi_output[0]);              /* 关闭了cgi_output中的读通道 */
+        close(cgi_input[1]);               /* 关闭了cgi_input中的写通道 */
         sprintf(meth_env, "REQUEST_METHOD=%s", method);
         putenv(meth_env);
         if (strcasecmp(method, "GET") == 0) {
@@ -385,21 +356,17 @@ void execute_cgi(int client, const char *path,
             putenv(length_env);
         }
         /**************************************************
-         * 表头文件#include<unistd.h>
-         * 定义函数
+         * #include<unistd.h>
          * int execl(const char * path,const char * arg,....);
-         * 函数说明
-         * execl()用来执行参数path字符串所代表的文件路径，接下来的参数代表执行该文件时传递过去的argv(0)、argv[1]……，最后一个参数必须用空指针(NULL)作结束。
-         * 返回值
+         * execl()用来执行参数path字符串所代表的文件路径，
+         * 接下来的参数代表执行该文件时传递过去的argv(0)、argv[1]……，最后一个参数必须用空指针(NULL)作结束。
          * 如果执行成功则函数不会返回，执行失败则直接返回-1，失败原因存于errno中。 
          * ***********************************************/
         execl(path, NULL);
         exit(0);
-    } else {    /* parent */
-        //关闭了cgi_output中的写通道，注意这是父进程中cgi_output变量和子进程要区分开
-        close(cgi_output[1]);
-        //关闭了cgi_input中的读通道
-        close(cgi_input[0]);
+    } else {    /* parent */        
+        close(cgi_output[1]);       /* 关闭了cgi_output中的写通道，注意这是父进程中cgi_output变量和子进程要区分开 */  
+        close(cgi_input[0]);        /* 关闭了cgi_input中的读通道 */
         if (strcasecmp(method, "POST") == 0)
             for (i = 0; i < content_length; i++) {
                 recv(client, &c, 1, 0);
@@ -411,10 +378,10 @@ void execute_cgi(int client, const char *path,
         close(cgi_output[0]);
         close(cgi_input[1]);
         /***************************************
-         * 定义函数：pid_t waitpid(pid_t pid, int * status, int options);
-         * 函数说明：waitpid()会暂时停止目前进程的执行, 直到有信号来到或子进程结束.
-         * 如果在调用wait()时子进程已经结束, 则wait()会立即返回子进程结束状态值. 子进程的结束状态值会由参数status 返回,
-         * 而子进程的进程识别码也会一快返回.
+         * pid_t waitpid(pid_t pid, int * status, int options);
+         * waitpid()会暂时停止目前进程的执行, 直到有信号来到或子进程结束.
+         * 如果在调用wait()时子进程已经结束, 则wait()会立即返回子进程结束状态值. 子进程的结束状态值会由参数status 
+         * 返回,而子进程的进程识别码也会一快返回.
          * 如果不在意结束状态值, 则参数status 可以设成NULL. 参数pid 为欲等待的子进程识别码, 其他数值意义如下：
          * 1、pid<-1 等待进程组识别码为pid 绝对值的任何子进程.
          * 2、pid=-1 等待任何子进程, 相当于wait().
@@ -424,7 +391,6 @@ void execute_cgi(int client, const char *path,
         waitpid(pid, &status, 0);
     }
 }
-
 /**********************************************************************/
 /* Get a line from a socket, whether the line ends in a newline,
  * carriage return, or a CRLF combination.  Terminates the string read
@@ -466,10 +432,8 @@ int get_line(int sock, char *buf, int size)
             c = '\n';
     }
     buf[i] = '\0';
-
     return(i);
 }
-
 /**********************************************************************/
 /* Return the informational HTTP headers about a file. */
 /* Parameters: the socket to print the headers on
@@ -574,10 +538,9 @@ int startup(u_short *port)
     httpd = socket(PF_INET, SOCK_STREAM, 0);
     if (httpd == -1)
         error_die("socket");
-    memset(&name, 0, sizeof(name)); //结构体清空置零
+    memset(&name, 0, sizeof(name)); /* 结构体清空置零 */
     name.sin_family = AF_INET;
-    //host to network, short 短整数从主机字节序到网络字节序转换
-    name.sin_port = htons(*port);
+    name.sin_port = htons(*port);   /* host to network, short 短整数从主机字节序到网络字节序转换 */
     /********************************
      * INADDR_ANY转换过来就是0.0.0.0，泛指本机的意思，也就是表示本机的所有IP
      * 因为有些机子不止一块网卡，多网卡的情况下，这个就表示所有网卡ip地址的意思。
@@ -589,7 +552,7 @@ int startup(u_short *port)
      * 
      * 所以出现INADDR_ANY，你只需绑定INADDR_ANY，管理一个套接字就行，不管数据是从哪个网卡过来的，只要是绑定的端口号过来的数据，都可以接收到。
      * ******************************/
-    name.sin_addr.s_addr = htonl(INADDR_ANY);   //host to network, long
+    name.sin_addr.s_addr = htonl(INADDR_ANY);           /* host to network, long */
     /******************************************
      * 套接字选项？？？？SO_REUSEADDR？？？？需要更多的研究，可写blog
      * int setsockopt(int socket, int level, int option_name,
@@ -631,7 +594,6 @@ int startup(u_short *port)
         error_die("listen");
     return(httpd);
 }
-
 /**********************************************************************/
 /* Inform the client that the requested web method has not been implemented.
  * Parameter: the client socket */
@@ -659,7 +621,6 @@ void unimplemented(int client)
 }
 
 /**********************************************************************/
-//开始源码阅读之路
 int main(void)
 {
     int server_sock = -1;
@@ -686,7 +647,6 @@ int main(void)
      * **************************/
     socklen_t  client_name_len = sizeof(client_name); 
     pthread_t newthread;
-
     /***************
      *  startup: 初始化 httpd 服务
      * 包括建立套接字，绑定端口，进行监听等。
@@ -706,15 +666,9 @@ int main(void)
         if (client_sock == -1)
             error_die("accept");
         /* accept_request(&client_sock); */
-        /***************************************
-         * int pthread_create(pthread_t *thread, pthread_attr_t *attr, void *(*start_routine)(void*), void *arg);
-         * 
-         * *************************************/
         if (pthread_create(&newthread , NULL, (void *)accept_request, (void *)(intptr_t)client_sock) != 0)
             perror("pthread_create");
     }
-
     close(server_sock);
-
     return(0);
 }
